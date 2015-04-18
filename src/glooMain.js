5bdf01b0c27a127c92e59850d395fa21f43db890
Kangaroopower <Kangaroopower@users.noreply.github.com>
2015-04-17 21:08:41 -0700

    variable fix

--- src/glooMain.js
+++ src/glooMain.js
@@ -777,7 +777,7 @@ igloo.extendProto(iglooRecentChanges, function () {
 
 						p = iglooF('contentManager').getPage(data[i].title);
 
-						if (!p) igloo.log('PAGE NOT FOUND- ', data[i].title, me.recentChanges, me.recentChanges[i], i);
+						if (!p) igloo.log('PAGE NOT FOUND- ', data[i].title, this.recentChanges, this.recentChanges[i], i);
 
 						p.addRevision(new iglooRevision(data[i]));
 						iglooF('contentManager').add(p, 'recentChanges');
